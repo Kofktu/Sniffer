f47b872ee338f5a3f8ea3ab9df06afdabdf972b4
kof.kofktu <kof.kofktu@kakaocorp.com>
2017-02-15 14:16:34 +0900

    project init

---
+++ Sniffer/SnifferTests/SnifferTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnifferTests.swift
+//  SnifferTests
+//
+//  Created by kofktu on 2017. 2. 15..
+//  Copyright © 2017년 Kofktu. All rights reserved.
+//
+
+import XCTest
+@testable import Sniffer
+
+class SnifferTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
